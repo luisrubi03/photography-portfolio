977039eeda8f6d59135d25d99f931680dcf49d3b
luisrubi03 <luisrubit@outlook.com>
2026-04-02 20:53:28 -0600

    funcion de upload y mejoras

--- app.py
+++ app.py
@@ -3,27 +3,54 @@ import requests
 from flask_session import Session
 import sqlite3 as sql
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+import os
+from datetime import timedelta, datetime
 app = Flask(__name__)
-app_secret_key = "secretkey123"
+app.secret_key = os.urandom(24)
 DATABASE= "users.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DATABASE
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=7)
 
 def create_tables():
     connection= sql.connect(DATABASE)
     cur = connection.cursor()
     cur.execute(''' CREATE TABLE IF NOT EXISTS users_data(
                     user_id INTEGER PRIMARY KEY AUTOINCREMENT,firstname TEXT NOT NULL, lastname TEXT NOT NULL,username TEXT NOT NULL,
-                    email TEXT NOT NULL,password TEXT NOT NULL, creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP)''')
-    connection.commit()
+                    email TEXT NOT NULL,password TEXT NOT NULL, creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, profile_picture TEXT NOT NULL)''')
     connection.close()
+
     print("Base de datos creada correctamente")
+
+def create_posts_table():
+    connection2 = sql.connect(DATABASE)
+    cur2 = connection2.cursor()
+    cur2.execute(''' CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, filename TEXT NOT NULL, user_id INTEGER NOT NULL, username TEXT NOT NULL, date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP NOT NULL)''')
+    connection2.close()
 
 @app.route("/")
 def index():
-    not session.get("user")
-    return redirect(url_for("login"))
-    return render_template('index.html')
+    if not session.get("username"):
+        return redirect(url_for('login'))
+
+    carpeta = app.config['UPLOAD_FOLDER']
+
+    connection = sql.connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute("SELECT filename, username FROM posts ORDER BY id DESC")
+    posts = cursor.fetchall()
+
+    posts_validos = []
+
+    for post in posts:
+        ruta = os.path.join(carpeta, post[0])
+        if os.path.exists(ruta):
+            posts_validos.append(post)
+
+    return render_template('index.html', show_navbar=True, posts=posts_validos)
 
 @app.route("/account")
 def account():
@@ -40,22 +67,25 @@ def load():
 @app.route("/login", methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
 
         connection = sql.connect(DATABASE)
         c = connection.cursor()
         c.execute("SELECT * FROM users_data WHERE username = ?", (username,))
         user = c.fetchone()
         connection.close()
+        #print(user)
+        print(session.get("username"))
+        if user and check_password_hash(user[5], password):
+            session["user_id"] = user[0]  # ID
+            session["username"] = user[3]  # username (mejor usar el de la BD)
 
-
-        if username:
-            session["username"] = username
             return redirect(url_for('index'))
         else:
-            return 'credenciales incorrectas'#hacer una pagina de errores comunes
-    return render_template('login.html')
+            return 'Credenciales incorrectas, intenta de nuevo'#hacer una pagina de errores comunes
+
+    return render_template('login.html', show_navbar=False)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -78,12 +108,12 @@ def register():
             return 'La confirmacion es necesaria'
 
         #variables de formularios
-        firstname = request.form.get('firstname')
-        lastname = request.form.get('lastname')
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        confirmation = request.form.get('confirmation')
+        firstname = request.form.get('firstname').strip()
+        lastname = request.form.get('lastname').strip()
+        username = request.form.get('username').strip()
+        email = request.form.get('email').strip()
+        password = request.form.get('password').strip()
+        confirmation = request.form.get('confirmation').strip()
 
         #verifica si la contrasena es igual a la confirmacion
         if password != confirmation:
@@ -110,7 +140,7 @@ def register():
 
         return redirect(url_for('login'))
 
-    return render_template('register.html')
+    return render_template('register.html', show_navbar=False)
 
 #funcion para recuperar contrasenas, aun en proceso
 @app.route("/recoverpassword", methods=['GET','POST'])
@@ -124,8 +154,53 @@ def recoverpassword():
 def logout():
     return redirect(url_for('login'))
 
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    print("SESSION:", session)
+
+    if request.method == 'POST':
+
+        if "user_id" not in session:
+            return redirect(url_for("login"))
+
+        if 'imagen' not in request.files:
+            return "No file"
+
+        file = request.files['imagen']
+
+        if file.filename == '':
+            return "No selected file"
+
+        filename = secure_filename(file.filename)
+
+        ruta = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(ruta)
+
+        connection = sql.connect(DATABASE)
+        c = connection.cursor()
+        if os.path.exists(ruta):
+            c.execute('''
+                      INSERT INTO posts (filename, user_id, username, date_time)
+                      VALUES (?, ?, ?, ?)
+                      ''', (filename, session["user_id"], session["username"], datetime.now()))
+
+            connection.commit()
+        else:
+            return "Error al guardar la imagen"
+
+        return redirect(url_for('upload'))
+
+    return render_template("upload.html", show_navbar= True)
+
+@app.route("/profile", methods=['GET', 'POST'])
+def profile():
+    return render_template("profile.html", show_navbar= True)
+
+
+
 if __name__ == "__main__":
     create_tables()
+    create_posts_table()
     connection = sql.connect(DATABASE)
     c = connection.cursor()
     #c.execute(''' -header -csv database.db "select * from credenciales;" > database.csv ''')
